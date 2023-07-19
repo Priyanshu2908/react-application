dcf66368918c544ef76d274fcded8a934599f128
Priyanshu2908 <prianshuchaubey29@gmail.com>
2023-07-19 12:45:25 +0530

    commit

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,12 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
-  return (
-    <div>
-        <h1>Login</h1>
+    const [likes, setlikes] = useState(0);
+    
+    const[imgUrl, setImgUrl] =useState('');
 
+
+    const selectfile=(e) =>{
+        const file= e.target.files[0];
+        console.log(file);
+        //reading file
+
+
+        
+    }
+  return (
+    
+   <div>
+     <div className='container'>
+        
+    <h1>Login</h1>
+   
+    <br />
+    <br />
+    
+
+    <h3>Email</h3>
+    <input className='email' type="text" /> 
+    <br />
+    <br />
+    <h3 className='password'>Password</h3>
+    <input type="number" /> 
+    <br />
+    <br />
+    <button className='btn btn-primary' onClick={()=>{setlikes(likes+1)}}>Sumit</button>
+   
+
+    
     </div>
+   </div>
   )
 }
 
-export default Login
+export default Login           
