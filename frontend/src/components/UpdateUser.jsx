6755bb910a667609f0befd07e03054270bad25ed
Priyanshu2908 <prianshuchaubey29@gmail.com>
2023-08-08 21:51:08 +0530

    updateuser

--- frontend/src/components/UpdateUser.jsx
+++ frontend/src/components/UpdateUser.jsx
@@ -1,25 +1,27 @@
-import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
-import Swal from "sweetalert2";
 import { useFormik } from "formik";
+import React, { useEffect } from "react";
+import { useState } from "react";
+import { useParams } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const UpdateUser = () => {
-  const{id}=useParams();
+  const { id } = useParams();
 
-  const [userData, setUserData] = useState({})
-  const fetchUserData = async()=>{
-    const res = await fetch('http://localhost:5000/user/getbyid/'+id);
+  const [userData, setUserData] = useState({});
+
+  const fetchUserData = async () => {
+    const res = await fetch("http://localhost:5000/user/getbyid/" + id);
     console.log(res.status);
 
     const data = await res.json();
     console.log(data);
-    setUserData(data)
-  }
+    setUserData(data);
+  };
 
-  useEffect(()=>{
+  useEffect(() => {
     fetchUserData();
+  }, []);
 
-  },[]);
   const signupForm = useFormik({
     initialValues: {
       name: '',
@@ -27,7 +29,7 @@ const UpdateUser = () => {
       password: ''
     },
 
-    onSubmit: async (values) => { 
+    onSubmit: async (values) => {
       console.log(values);
 
       // sending request to backend
@@ -44,10 +46,10 @@ const UpdateUser = () => {
       if(res.status === 200){
         Swal.fire({
           icon : 'success',
-          title : 'Updated Success!!',
-          text: 'User Updated Sucessfullly'
+          title : 'Update Success!!',
+          text: 'User Updated successfully'
         });
-      
+        
       }else{
         Swal.fire({
           icon : 'error',
@@ -60,7 +62,7 @@ const UpdateUser = () => {
 
   return (
     <div>
-      <h1 className='text-center'>Update User Data</h1>
+      <h1 className="text-center">Update User Data</h1>
       <h2>{id}</h2>
       <div className="card w-25 shadow-lg rounded-5">
         <div className="card-body p-5">
@@ -73,8 +75,6 @@ const UpdateUser = () => {
             <input className="form-control mb-4 rounded-5" type="email" name="email" onChange={signupForm.handleChange} value={signupForm.values.email} />
             <label htmlFor="">Password</label>
             <input className="form-control mb-4 rounded-5" type="password" name="password" onChange={signupForm.handleChange}  value={signupForm.values.password} />
-            
-           
 
             <button className="btn btn-danger w-100 mt-4 rounded-5">
               Signup
@@ -83,7 +83,7 @@ const UpdateUser = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default UpdateUser
+export default UpdateUser;
